3d4d781ef558e3e4ae4f98f08f858289c067656c
Charlie Griefer <Charlie@Charlies-MacBook-Pro.local>
2013-04-27 00:14:16 -0700

    Updated project.clj file

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject my-4clojure-solutions "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "My solutions to the problems found at http://www.4clojure.com"
+  :url "https://github.com/charliegriefer/my-4clojure-solutions"
   :dependencies [[org.clojure/clojure "1.4.0"]])
