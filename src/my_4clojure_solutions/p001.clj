b69151ee7b57cc9321f15952fd5d0f40cafd415a
Charlie Griefer <Charlie@Charlies-MacBook-Pro.local>
2013-04-27 00:14:37 -0700

    Question 1.

---
+++ src/my_4clojure_solutions/p001.clj
@@ -0,0 +1,22 @@
+;; http://www.4clojure.com/problem/1
+
+;; Nothing but the Truth
+;; This is a clojure form. Enter a value which will make the form evaluate to true.
+
+;; (= ___ true)
+
+;; Answer:
+;; true
+
+;; Output:
+(= true true)
+
+;; Rationale:
+;; true equals itself. Clojure does not do implicit boolean conversion.
+;; (= 1 true) ;false
+;; (= "true" true) ;false
+;; In order to coerce a non-boolean value to a boolean value, use (boolean)
+;; (= (boolean 1) true) ;true
+;; (= (bolean "true") true) ;true
+;; (= (boolean nil) true) ;false
+;; All values are truthy except for nil.
