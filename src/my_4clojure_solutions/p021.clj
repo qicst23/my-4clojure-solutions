387689d19f40ef9f138a818526e0a1c511fa1c40
Charlie Griefer <Charlie@Charlies-MacBook-Pro.local>
2013-04-28 23:01:00 -0700

    Removed the name of the anonymous function. Using recur, so name is not needed. Altho, not sure if recur is the right choice here.

--- src/my_4clojure_solutions/p021.clj
+++ src/my_4clojure_solutions/p021.clj
@@ -15,10 +15,10 @@
 ;; '(20 30 40)
 
 ;; Output:
-(= ((fn my-nth [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) '(4 5 6 7) 2) )
-(= ((fn my-nth [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) [:a :b :c] 0) :a)
-(= ((fn my-nth [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) [1 2 3 4] 1) 2)
-(= ((fn my-nth [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) '([1 2] [3 4] [5 6]) 2) [5 6])
+(= ((fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) '(4 5 6 7) 2) )
+(= ((fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) [:a :b :c] 0) :a)
+(= ((fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) [1 2 3 4] 1) 2)
+(= ((fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) '([1 2] [3 4] [5 6]) 2) [5 6])
 
 ;; Explanation:
 ;; This will likely end up being a "you could also have done...", as
