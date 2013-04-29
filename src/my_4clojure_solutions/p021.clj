3b7a378a8c3384f2ea50331f9c56867a47d230cc
Charlie Griefer <Charlie@Charlies-MacBook-Pro.local>
2013-04-29 10:23:02 -0700

    Accepted my final answer for Problem 21

--- src/my_4clojure_solutions/p021.clj
+++ src/my_4clojure_solutions/p021.clj
@@ -12,15 +12,26 @@
 ;; (= (__ '([1 2] [3 4] [5 6]) 2) [5 6])
 
 ;; Answer:
-;; 
+(fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1))))
 
 ;; Output:
-(= ((fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) '(4 5 6 7) 2) )
+(= ((fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) '(4 5 6 7) 2) 6)
 (= ((fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) [:a :b :c] 0) :a)
 (= ((fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) [1 2 3 4] 1) 2)
 (= ((fn [coll n] (if (zero? n) (first coll) (recur (rest coll) (- n 1)))) '([1 2] [3 4] [5 6]) 2) [5 6])
 
 ;; Explanation:
-;; This will likely end up being a "you could also have done...", as
-;; I'm fairly certain this solution is more complex than what the
-;; 4Clojure folks are looking for.
+;; At first I didn't think this was a good solution, as it was the
+;; first of the 4Clojure problems that used recursion. I thought there
+;; must be a simpler way. But I am going to stick with this.
+;; The anonymous function takes 2 arguments. A collection (coll), and the
+;; index position of the element to return (n). If n is 0, return the
+;; first element of the collection. Otherwise, use recur to
+;; recursively call the anonymous function passing the rest of the
+;; collection and n - 1.
+;; It's also possible to name the anonymous function and call it
+;; explicitly recursively. But using recur provides tail call
+;; optimization, and according to the docs, if the recursion is in
+;; tail position, is encouraged in clojure. That function would look like:
+;; (fn my-nth [coll n] (if (zeron? n) (first coll) (my-nth (rest coll) (-n 1))))
+
