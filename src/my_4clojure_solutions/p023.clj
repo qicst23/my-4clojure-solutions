165e53b5565c3670c6cf2d39f1a414caf91e03f6
Charlie Griefer <Charlie@Charlies-MacBook-Pro.local>
2013-04-29 23:06:29 -0700

    Added Problem 23 (solved)

--- src/my_4clojure_solutions/p023.clj
+++ src/my_4clojure_solutions/p023.clj
@@ -12,12 +12,12 @@
 ;; (= (__ [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
 
 ;; Answer:
-
+#(reduce conj () %)
 
 ;; Output:
-(= (__ [1 2 3 4 5]) [5 4 3 2 1])
-(= (__ (sorted-set 5 7 2 7)) '(7 5 2))
-(= (__ [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
+(= (#(reduce conj () %) [1 2 3 4 5]) [5 4 3 2 1])
+(= (#(reduce conj () %) (sorted-set 5 7 2 7)) '(7 5 2))
+(= (#(reduce conj () %) [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
 
 ;; Explanation:
 ;; Started off with #(repeatedly (count %) (cons (last %) %)), but
@@ -27,3 +27,11 @@
 ;; will work, but it's alpha. In any event, cons has no such
 ;; corresponding function. Still working on this one.
 
+;; Back. Solution is up above. I cheated (again). I looked at the
+;; source of reverse in the REPL via (source reverse). I used that as
+;; my answer. One thing worth noting... I was thrown because the first
+;; answer looked like it returned a vector. So I was trying to ensure
+;; the same type of collection. Altho in hindsight, that was silly,
+;; because the second problem clearly returns a list and not a set.
+
+;; I am ashamed.
